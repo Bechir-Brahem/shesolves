4be8abb4cc65d5d1e46e779733717d6aba90c76c
maaref omar <64233476+omarmaaref@users.noreply.github.com>
2021-02-04 23:12:36 +0100

    Add files via upload

--- factorial/fact.cpp
+++ factorial/fact.cpp
@@ -2,21 +2,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
-void solve() {
+int main(){
 	int n,result = 0;
 	cin>>n;
     for(long long i=5; n/i>0; i*=5){
         result += (n/i);
     }
     cout<<result<<endl;
-}
-
-int main(){
-	int t;
-	cin>>t;
-	while(t--){
-		solve();
-		}
 	}
