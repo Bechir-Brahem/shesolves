d315192ecd828d696841e4a4197184920a4979a9
MazenIss <mazenissaoui@insat.u-carthage.tn>
2021-02-18 20:07:43 +0100

    Add files via upload

---
+++ Only One Clue/onlyoneclue.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+ using namespace std;
+ void test(){
+     int k,x;
+     cin>>k>>x;
+     for(size_t i=x-k+1;i<x+k;i++)
+     cout<<i<<" ";
+
+ }
+ int main(){ 
+     test();
+     return 0;
+ }
