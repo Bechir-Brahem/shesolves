00a0f4ee1bb0fa382f2ddc922b232c11b03e6686
alabh <alabenhmouda@gmail.com>
2021-02-19 16:35:35 +0100

    correct onlyoneclue.cpp solution that used size_t instead of int

--- Only One Clue/onlyoneclue.cpp
+++ Only One Clue/onlyoneclue.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
- using namespace std;
- void test(){
-     int k,x;
-     cin>>k>>x;
-     for(size_t i=x-k+1;i<x+k;i++)
-     cout<<i<<" ";
-
- }
- int main(){ 
-     test();
-     return 0;
- }
+using namespace std;
+void test() {
+    int k, x;
+    cin >> k >> x;
+    for (int i = x - k + 1; i < x + k; i++)
+        cout << i << " ";
+}
+int main() {
+    test();
+    return 0;
+}
