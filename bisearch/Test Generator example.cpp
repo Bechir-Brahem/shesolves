f9e917427da4e8846597bdaa794758156e4a300e
alabh <alabenhmouda@gmail.com>
2021-02-19 18:29:13 +0100

    add tests and answer files to bisearch

---
+++ bisearch/Test Generator example.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int randomInt(int l, int r) { return uniform_int_distribution<int>(l, r)(rng); }
+
+string randomString(int n, char l, char r) {
+    int len = r - l + 1;
+    string res;
+    for (int i = 1; i <= n; ++i)
+        res += char(l + randomInt(0, len - 1));
+    return res;
+}
+
+int digitsNbr(int x) {
+    if (x == 0) {
+        return 1;
+    }
+    int ret{};
+    while (x != 0) {
+        ret++;
+        x /= 10;
+    }
+    return ret;
+}
+
+const char nl = '\n';
+
+int main() {
+    const int tests = 120;
+    const int width = digitsNbr(tests);
+    for (int i = 113; i <= tests; ++i) {
+        stringstream ss;
+        const int testWidth = digitsNbr(i);
+        string testNumber(width - testWidth, '0');
+        char *testNumberStr = new char[testWidth + 1];
+        sprintf(testNumberStr, "%d", i);
+        testNumber += testNumberStr;
+        ss << "tests/test" << testNumber << ".in";
+        delete[] testNumberStr;
+
+        char fileName[20];
+        ss >> fileName;
+        freopen(fileName, "w", stdout);
+
+        // ------------------------------------------ CHANGE THIS
+        int n = 100000;
+        int s = 1000000;
+        cout << n << ' ' << s << nl;
+        int j;
+        for (j = 0; j < n - 1; j++) {
+            cout << randomInt(1, 10) << ' ';
+        }
+        cout << 1000000 - 1 << nl;
+    }
+}
