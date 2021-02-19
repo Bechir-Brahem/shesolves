c8e76771f589db281ac32722c857c8b60d437f03
alabh <alabenhmouda@gmail.com>
2021-02-19 16:50:04 +0100

    produce answer files for the ink problem

--- ink_problem/ink_siraj.cpp
+++ ink_problem/ink_siraj.cpp
@@ -1,39 +1,39 @@
-//Problem 2 : The Ink Problem
+// Problem 2 : The Ink Problem
 #include <bits/stdc++.h>
 using namespace std;
-#pragma GCC target ("avx2")
-#pragma GCC optimization ("O3")
-#pragma GCC optimization ("unroll-loops")
+#pragma GCC target("avx2")
+#pragma GCC optimization("O3")
+#pragma GCC optimization("unroll-loops")
 #define PI = arccos(-1)
 typedef long long ll;
 typedef long double ld;
-const int INF=1e9;
-const ll LINF=1e18;
+const int INF = 1e9;
+const ll LINF = 1e18;
 const int mod = 1e9 + 7;
 
 int main() {
-#ifndef ONLINE_JUDGE 
-freopen("input.txt","r",stdin);
-freopen("output.txt","w",stdout);
+#ifndef ONLINE_JUDGE
+/* freopen("input.txt","r",stdin); */
+/* freopen("output.txt","w",stdout); */
 #endif
-ios::sync_with_stdio(false);
-cin.tie(nullptr);
-cout.tie(nullptr);
-int n,l,i=0,ans=0,sum=0;
-cin>>n>>l;
-int arr[n];
-for(;i<n;++i) 
-    cin>>arr[i];
-sort(arr,arr+n);
-for(i=0;i<n && sum<=l;++i){
-    sum+=arr[i];
-    ans++;
-}
-if(sum>l){
-    sum-=arr[i-1];
-    ans--;
-}
-cout<<ans;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int n, l, i = 0, ans = 0, sum = 0;
+    cin >> n >> l;
+    int arr[n];
+    for (; i < n; ++i)
+        cin >> arr[i];
+    sort(arr, arr + n);
+    for (i = 0; i < n && sum <= l; ++i) {
+        sum += arr[i];
+        ans++;
+    }
+    if (sum > l) {
+        sum -= arr[i - 1];
+        ans--;
+    }
+    cout << ans;
 
-return 0;
+    return 0;
 }
