e06edf3b42dab49b2bc79564fcb2927345d7b51d
alabh <alabenhmouda@gmail.com>
2021-02-19 12:33:27 +0100

    correct solution's output for 5dinars problem

--- 5 dinar coins/sol.cpp
+++ 5 dinar coins/sol.cpp
@@ -2,20 +2,17 @@
 using namespace std;
 #define ll long long int
 
-void solve()
-{
-    ll K,S;
-    cin>>K>>S;
-    cout<<(K*5>=S?"YES":"NO")<<endl;
+void solve() {
+    ll K, S;
+    cin >> K >> S;
+    cout << (K * 5 >= S ? "Rich!" : "Not yet") << endl;
 }
 
-int main()
-{
-	int t = 1;
-	//cin >> t;
-	for (int i = 1; i <= t; i++)
-	{
-		solve();
-	}
-	return 0;
+int main() {
+    int t = 1;
+    // cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve();
+    }
+    return 0;
 }
